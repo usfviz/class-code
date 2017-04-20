7449188f768ba215175638511d0c02080a981de4
Yannet <yannet@gmail.com>
2017-04-20 11:07:32 -0700

    updated version

--- sfschools-basic/app.R
+++ sfschools-basic/app.R
@@ -8,12 +8,14 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
-  d <- read.csv("schools_math_eng_scores.csv")
+  d <- read.csv("../schools_math_eng_scores.csv")
   output$mymap <- renderLeaflet({
   m <- leaflet() %>%
     addTiles() %>%
-    addMarkers(data = d, lng = ~longitude, lat = ~latitude,
-               popup = ~paste("<h4>", school_name, "</h4>",
+    addCircleMarkers(data = d, lng = ~longitude, lat = ~latitude,
+                     color = "#46673b", fillOpacity = 0.7, 
+                     radius = ~1.2*score,
+                     popup = ~paste("<h4>", school_name, "</h4>",
                               "Score: ", score, sep = ""))
   })
    
